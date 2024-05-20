160e1d850a7cf387ea68d09fca991a301897d884
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-05-20 17:24:00 +0530

    Update video_converter.py

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -50,7 +50,7 @@ async def convert_to_video(bot, update):
             await update.reply_text(
                 text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="Join My Support Group", url=f"https://t.me/Anity_botsupport")]
               ])
             )
             return  
