7c196a88f149e1f48105c77e5ec089ba0929ddad
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-05-20 17:20:17 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -107,7 +107,7 @@ async def start_me(bot, update):
                 ],
                 [
                     InlineKeyboardButton('Other Bots', url='https://t.me/Mikeyfile_bot'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Source', url='https://github.com/saimshamim8423/FileRenameBot')
                 ]
             ]
         ),
